49e5bc721a3d2b84db38ef5d65104b0f347751f9
Ceci Benitez <cecibenitez@Cecis-MacBook-Pro.local>
2022-05-30 11:07:57 -0400

    Added CORS policies and configuration

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+let allowed = ["http://localhost:3000", "other link"];
+
+function options(req, res) {
+  let tmp;
+  let origin = req.header("Origin");
+
+  if (allowed.indexOf(origin) > -1) {
+    tmp = {
+      origin: true,
+      optionSuccessStatus: 200,
+    };
+  } else {
+    tmp = {
+      origin: "No",
+    };
+  }
+
+  res(null, tmp);
+}
 
 const app = express();
+app.use(cors(options));
 
 app.get("/", (req, res) => {
   res.send("welcome from home");
