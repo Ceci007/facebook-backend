6fb3ae884c1aad29d2576f41b3364377a2275fca
Ceci Benitez <cecibot9@gmail.com>
2022-06-20 15:00:04 -0400

    Added reset code functionality

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ const {
   activateAccount,
   login,
   sendVerification,
+  findUser,
+  sendResetPasswordCode,
 } = require("../controllers/user");
 const { authUser } = require("../middlewares/auth");
 
@@ -13,5 +15,7 @@ router.post("/register", register);
 router.post("/activate", authUser, activateAccount);
 router.post("/login", login);
 router.post("/sendVerification", authUser, sendVerification);
+router.post("/findUser", findUser);
+router.post("/sendResetPasswordCode", sendResetPasswordCode);
 
 module.exports = router;
